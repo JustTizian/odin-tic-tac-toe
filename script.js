27f0e621b6afed80a620049883265b39656573e1
tihi <77891364+JustTizian@users.noreply.github.com>
2025-09-05 16:09:02 +0200

    Finish console version of game, no reset yet

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 const gameboard = (function () {
-    const gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const gameboard = Array(9).fill(null)
 
     function place(index) {
-        gameboard[index-1] = game.getActivePlayer()
-        game.swapActivePlayer()
+        if (gameboard[index] === "X" || gameboard[index] === "O") return false;
+        gameboard[index] = game.getActivePlayer();
+        return true;
     }
 
     function displayGameBoard() {
         console.dir(gameboard)
     }
 
+    function checkWin() {
+        const winningPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+
+        for (let i = 0; i < winningPatterns.length; i++) {
+            const [a, b, c] = winningPatterns[i]
+            if (gameboard[a] === null) continue;
+            if (gameboard[a] === gameboard[b] &&
+                gameboard[b] === gameboard[c]) {
+                return gameboard[a]
+            }
+        }
+    }
+
+    function checkTie(){
+        return gameboard.every(cell => cell !== null)
+    }
+
     return {
         place,
-        displayGameBoard
+        displayGameBoard,
+        checkWin,
+        checkTie
     }
 })()
 
@@ -20,9 +44,25 @@ const gameboard = (function () {
 const game = (function () {
     let isXTurn = true;
 
-    function playTurn(index){
-        gameboard.place(index)
+    function playTurn(index) {
+        while (gameboard.place(index)) {
+            gameboard.place(index)
+        }
         gameboard.displayGameBoard()
+        let winner = gameboard.checkWin()
+        if (winner) {
+            console.log(`${winner} won!`)
+            return;
+        }
+
+        if(gameboard.checkTie()){
+            console.log("Tie")
+            return;
+        }
+        
+        game.swapActivePlayer()
+        
+        
     }
 
     return {
@@ -31,3 +71,7 @@ const game = (function () {
         playTurn
     }
 })()
+
+/* const UIHandler = function(){
+
+}() */

d7d11bd05ea83b92768ab1e55d3301a5d886ba4f
tihi <77891364+JustTizian@users.noreply.github.com>
2025-09-05 19:44:25 +0200

    finish console version without reset function

--- script.js
+++ script.js
@@ -1,46 +1,104 @@
 const Board = (() => {
     const board = Array(9).fill(null);
 
-    function setMarker(index, marker){
+    function setMarker(index, marker) {
+        if(board[index]) return false;
         board[index] = marker;
         logBoard()
+        return true;
     }
 
-    function logBoard(){
+    function checkWin() {
+        const winningPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+
+        for (let i = 0; i < winningPatterns.length; i++) {
+            const [a, b, c] = winningPatterns[i]
+            if (board[a] === null) continue;
+            if (board[a] === board[b] && board[b] === board[c]) {
+                return board[a];
+            }
+        }
+    }
+
+    function checkTie() {
+        return board.every(cell => cell)
+    }
+
+    function logBoard() {
         console.dir(board)
     }
 
-    return{
-        setMarker
+    return {
+        setMarker,
+        checkWin,
+        checkTie
     }
 })()
 
 const Game = (() => {
-    const player1 = player("Hänsel", "X");
-    const player2 = player("Gretel", "O");
-    let activePlayer = player1;
+    const players = [
+        player("Hänsel", "X"),
+        player("Gretel", "O")
+    ]
 
-    function playTurn(index){
-        Board.setMarker(index, activePlayer.marker)
+    let activePlayer = players[0];
+
+    function playTurn(index) {
+        if(!Board.setMarker(index, activePlayer.marker)){
+            return
+        }
+        
+        const winner = getPlayerByMarker(Board.checkWin())
+        if (winner) {
+            console.log(winner)
+            return;
+        }
+        if (Board.checkTie()) {
+            console.log("Tie")
+            return;
+        }
+
         swapActivePlayer();
     }
 
-    function swapActivePlayer(){
-        console.log(activePlayer == player1)
-        console.log(activePlayer == player2)
-        activePlayer = activePlayer === player1 ? player2 : player1;
+    function swapActivePlayer() {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
     }
 
-    return{
+    function getPlayerByMarker(marker) {
+        return players.find(player => player.marker === marker)
+    }
+
+    return {
         playTurn
     }
-    
+
 })()
 
-function player(name, marker){
-    return{name, marker};
+function player(name, marker) {
+    return { name, marker };
 }
 
 const UI = (() => {
+    const gameBoardContainer = $(".game-board-containter")
 
+
+    function $(query){
+        return document.querySelector(query)
+    }
+
+
+    function createBoard(){
+
+    }
+    
+    
+    
+    function renderBoard(){
+
+    }
 })()
